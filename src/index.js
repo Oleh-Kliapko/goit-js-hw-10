dfa1d5b8517a38db53aa3d6c57fd2e17502f285c
Oleh Kliapko <goldclub1977@gmail.com>
2022-11-08 21:42:11 +0200

    update Task

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import { fetchCountries } from '../src/fetchCountries';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix';
 
-// console.log(Notify.failure('Oops, there is no country with that name'))
+// Notify.failure('Oops, there is no country with that name')
 
 const DEBOUNCE_DELAY = 300;
+const TIMEOUT_NOTIFICATION = 4000;
 
 const refs = {
   inputEl: document.querySelector('#search-box'),
@@ -27,41 +28,51 @@ function onSearchCountry(evt) {
     Notify.warning(
       'Too many matches found. Please enter a more specific name',
       {
-        timeout: 5000,
+        timeout: TIMEOUT_NOTIFICATION,
       }
     );
     return;
   } else if (evt.target.value.length === 0) {
     Notify.info('Please start entering some country for searching', {
-      timeout: 5000,
+      timeout: TIMEOUT_NOTIFICATION,
     });
+    refs.countriesListEl.innerHTML = '';
+    refs.infoAboutCountryEl.innerHTML = '';
     refs.inputEl.removeEventListener('keydown', evt);
     return;
   }
 
   fetchCountries(evt.target.value)
     .then(onRenderCountriesList)
-    .catch(error => console.log(error));
+    .catch(Notify.failure('Oops, there is no country with that name'), {
+      timeout: TIMEOUT_NOTIFICATION,
+    });
 }
 
 function onRenderCountriesList(countries) {
-  console.log('🚀 ~ countries', countries);
   const numberCountriesFound = countries.length;
-  const markupCountriesList = countries.map(
-    country =>
-      `<li><img src="${country.flags.svg}"
-      alt="Flag of country ${country.name.official}" />
+
+  const markupCountriesList = countries
+    .map(
+      country =>
+        `<li class="country"><img src="${country.flags.svg}"
+      alt="Flag of ${country.name.official}" />
       <h1>${country.name.official}</h1></li>`
-  );
+    )
+    .join('');
   refs.countriesListEl.innerHTML = markupCountriesList;
 
   if (numberCountriesFound === 1) {
-    const markupInfoAboutCountry = countries.map(
-      country =>
-        `<p><b>Capital: </b>${country.capital}</p>
+    const markupInfoAboutCountry = countries
+      .map(
+        country =>
+          `<p><b>Capital: </b>${country.capital}</p>
          <p><b>Population: </b>${country.population}</p>
-         <p><b>Languages: </b>${country.languages}</p>`
-    );
+         <p><b>Languages: </b>${Object.values(country.languages)}</p>`
+      )
+      .join('');
     refs.infoAboutCountryEl.innerHTML = markupInfoAboutCountry;
+    return;
   }
+  refs.infoAboutCountryEl.innerHTML = '';
 }
